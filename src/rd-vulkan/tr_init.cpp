c8807e3c73f7612fdf33621e9f12e1040677a062
JKSunny <60109631+JKSunny@users.noreply.github.com>
2025-10-11 11:58:46 +0200

    Vulkan: Disable vbo model caching by default

--- src/rd-vulkan/tr_init.cpp
+++ src/rd-vulkan/tr_init.cpp
@@ -964,7 +964,7 @@ void R_Register( void )
 	//ri.Cvar_CheckRange(r_nomip, 0, 1, qtrue);
 #ifdef USE_VBO
 	r_vbo								= ri.Cvar_Get("r_vbo",								"1",						CVAR_ARCHIVE | CVAR_LATCH );
-	r_vbo_models						= ri.Cvar_Get("r_vbo_models",						"1",						CVAR_ARCHIVE | CVAR_LATCH );
+	r_vbo_models						= ri.Cvar_Get("r_vbo_models",						"0",						CVAR_ARCHIVE | CVAR_LATCH );
 #endif
 	r_renderWidth						= ri.Cvar_Get("r_renderWidth",						"800",						CVAR_ARCHIVE_ND | CVAR_LATCH );
 	r_renderHeight						= ri.Cvar_Get("r_renderHeight",						"600",						CVAR_ARCHIVE_ND | CVAR_LATCH );
