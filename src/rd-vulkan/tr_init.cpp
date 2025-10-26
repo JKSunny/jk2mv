e2e2a5c44cd21856d1b39fa4ffc000db190a0b8a
JKSunny <60109631+JKSunny@users.noreply.github.com>
2025-10-26 13:09:06 +0100

    Try include path for g2_local.h in tr_init.cpp

--- src/rd-vulkan/tr_init.cpp
+++ src/rd-vulkan/tr_init.cpp
@@ -29,7 +29,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #include "../rd-common/tr_common.h"
 #include "tr_WorldEffects.h"
 #include "qcommon/MiniHeap.h"
-#include "ghoul2/g2_local.h"
+#include "../ghoul2/g2_local.h"
 
 glconfig_t	glConfig;
 glconfigExt_t glConfigExt;
